6a1ce8d5d064f1402b4c36e574e83e307b8d74cc
Dennie Vilumsons <dennie@brainworks.se>
2013-09-26 21:25:41 +0200

    V1

---
+++ js/brainworks.js
@@ -0,0 +1,7 @@
+$(document).bind("mobileinit", function () {
+    $.mobile.ajaxEnabled = false;
+    $.mobile.phonegapNavigationEnabled = true;
+    $.mobile.pushStateEnabled = false;
+    $.mobile.hashListeningEnabled = false;
+    
+});
